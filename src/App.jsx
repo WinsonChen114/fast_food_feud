63f277c5d634c77bd83142a0b4f3af99352357fe
WinsonChen114 <14winsonchen@gmail.com>
2022-06-13 13:59:49 -0400

    Displayed Instruction, Header, Category, Restaurants

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import * as React from "react"
 // IMPORT ANY NEEDED COMPONENTS HERE
 import { createDataSet } from "./data/dataset"
 import "./App.css"
+import Header from "./components/Header/Header.jsx"
+import Instructions from "./components/Instructions/Instructions"
 
 // don't move this!
 export const appInfo = {
@@ -27,21 +29,26 @@ export function App() {
       <div className="CategoriesColumn col">
         <div className="categories options">
           <h2 className="title">Categories</h2>
-          {/* YOUR CODE HERE */}
+          {categories.map((category, index) => (
+            <p key={index}>{category}</p>
+          ))}
         </div>
       </div>
 
       {/* MAIN COLUMN */}
       <div className="container">
-        {/* HEADER GOES HERE */}
+        <Header title={appInfo.title} tagline={appInfo.tagline} description={appInfo.description} />
 
         {/* RESTAURANTS ROW */}
         <div className="RestaurantsRow">
           <h2 className="title">Restaurants</h2>
-          <div className="restaurants options">{/* YOUR CODE HERE */}</div>
+          <div className="restaurants options">
+            {restaurants.map((restaurant, index) => (
+              <p key={index}>{restaurant}</p>
+            ))}</div>
         </div>
 
-        {/* INSTRUCTIONS GO HERE */}
+        <Instructions instructions={appInfo.instructions.start} />
 
         {/* MENU DISPLAY */}
         <div className="MenuDisplay display">
