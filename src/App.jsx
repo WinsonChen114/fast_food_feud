50ce7fc725a8bb404da6706b3de2d754c7ffacb0
WinsonChen114 <14winsonchen@gmail.com>
2022-06-14 14:26:03 -0400

    Changed Instructions based on user selections

--- src/App.jsx
+++ src/App.jsx
@@ -24,25 +24,93 @@ export const appInfo = {
 // or this!
 const { data, categories, restaurants } = createDataSet()
 
+//booleans for instruction state
+let catebool = false
+let restbool = false
+let itembool = false
+
 export function App() {
   //States for Category and restarurant
-  const [selectedCategory, setCategory] = React.useState()
-  const [selectedRestaurant, setRestaurant] = React.useState()
-  const [selectedMenuItem, setMenuItem] = React.useState()
+  const [selectedCategory, setCategory] = React.useState(null)
+  const [selectedRestaurant, setRestaurant] = React.useState(null)
+  const [selectedMenuItem, setMenuItem] = React.useState(null)
+  const [instructionState, setInstructionState] = React.useState("start")
 
+  //Selecting a Chip
   const cateOnClick = (category) => {
     setMenuItem(null)
+    catebool = true
+    itembool = false
     setCategory(category)
+    instrOnClick()
+    console.log("click fire")
   }
 
   const restOnClick = (restaurant) => {
     setMenuItem(null)
+    restbool = true
+    itembool = false
     setRestaurant(restaurant)
+    instrOnClick()
   }
 
   const itemOnClick = (item) => {
+    itembool = true
     setMenuItem(item)
+    instrOnClick()
   }
+
+  //Setting Instrction state
+  const instrOnClick = () => {
+    if (catebool) {
+      if (restbool) {
+        //All are selected
+        if (itembool) {
+          setInstructionState("allSelected")
+        }
+        //Item has not been selected
+        else {
+          setInstructionState("noSelectedItem")
+        }
+      }
+      //Only category is selected
+      else {
+        setInstructionState("onlyCategory")
+      }
+    }
+    else {
+      //Only restaurant is selected
+      if (restbool) {
+        setInstructionState("onlyRestaurant")
+      }
+      //Nothing is selected
+      else {
+        setInstructionState("start")
+      }
+    }
+  }
+
+  //Deselecting a Chip
+  const cateClose = () => {
+    catebool = false
+    itembool = false
+    setMenuItem(null)
+    setCategory(null)
+    console.log("close fire")
+  }
+
+  const restClose = () => {
+    restbool = false
+    itembool = false
+    setMenuItem(null)
+    setRestaurant(null)
+  }
+
+  const itemClose = () => {
+    itembool = false
+    setMenuItem(null)
+  }
+
 
   const currentMenuItems = data.filter((item) => { return item.food_category == selectedCategory && item.restaurant == selectedRestaurant })
 
@@ -53,7 +121,7 @@ export function App() {
         <div className="categories options">
           <h2 className="title">Categories</h2>
           {categories.map((category) => (
-            <Chip label={category} key={category} onClick={() => cateOnClick(category)} isActive={selectedCategory == category} />
+            <Chip label={category} key={category} onClick={() => cateOnClick(category)} close={() => cateClose} isActive={selectedCategory == category} />
           ))}
         </div>
       </div>
@@ -67,19 +135,19 @@ export function App() {
           <h2 className="title">Restaurants</h2>
           <div className="restaurants options">
             {restaurants.map((restaurant) => (
-              <Chip label={restaurant} key={restaurant} onClick={() => restOnClick(restaurant)} isActive={selectedRestaurant == restaurant} />
+              <Chip label={restaurant} key={restaurant} onClick={() => restOnClick(restaurant)} close={() => restClose} isActive={selectedRestaurant == restaurant} />
             ))}
           </div>
         </div>
 
-        <Instructions instructions={appInfo.instructions.start} />
+        <Instructions instructions={appInfo.instructions[instructionState]} />
 
         {/* MENU DISPLAY */}
         <div className="MenuDisplay display">
           <div className="MenuItemButtons menu-items">
             <h2 className="title">Menu Items</h2>
             {currentMenuItems.map((item) => (
-              <Chip label={item.item_name} key={item.item_description} onClick={() => itemOnClick(item)} isActive={ selectedMenuItem && selectedMenuItem.item_name == item.item_name} />
+              <Chip label={item.item_name} key={item.item_description} onClick={() => itemOnClick(item)} close={() => itemClose} isActive={selectedMenuItem && selectedMenuItem.item_name == item.item_name} />
             ))}
           </div>
 
--- src/components/Chip/Chip.jsx
+++ src/components/Chip/Chip.jsx
@@ -1,13 +1,13 @@
 import * as React from "react"
 import "./Chip.css"
 
-export function Chip({ label = "", isActive = false, onClick = () => {}}) {
+export function Chip({ label = "", isActive = false, onClick = () => {}, close = () => {}}) {
   let buttonClassName = isActive ? "chip active" : "chip"
 
   return (
     <button className={buttonClassName} onClick={onClick}>
       <p className="label">{label}</p>
-      <span className="close" role="button">{`X`}</span>
+      <span className="close" role="button" tabIndex = "0" onClick={close}>{`X`}</span>
     </button>
   )
 }
